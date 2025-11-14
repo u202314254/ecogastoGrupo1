a5314743381a6cec5c4d31dfd593afae731932f0
DilanAnton <u202312076@upc.edu.pe>
2025-11-14 02:28:39 -0500

    Refactor Usuario class into Perfil class

--- src/app/models/Perfil.ts
+++ src/app/models/Perfil.ts
@@ -1,10 +1,10 @@
-import { Hogar } from './Hogar';
+import { Usuario } from "./Usuario";
 
-export class Usuario{
-    idUsuario:number=0
-    hogar:Hogar=new Hogar()
-    username:string=""
-    correo:string=""
-    password:string=""
-    estado:boolean=false
+export class Perfil{
+    idPerfil:number=0
+    usuario:Usuario=new Usuario()
+    nombre:string=""
+    edad:number=0
+    genero:string=""
+    telefono:string=""
 }
