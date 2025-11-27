f32ff4f6e764ee100822cfa76a33b3deec5fd15d
JefreyDA <u202318818@upc.edu.pe>
2025-11-26 22:42:43 -0500

    Correccion

--- src/app/models/Insignia.ts
+++
@@ -1,9 +0,0 @@
-import { Meta } from './Meta';
-
-export class Insignia{
-    idInsignia:number=0
-    meta: Meta = new Meta()
-    nombre_logro:string=""
-    descripcion:string=""
-    puntos:number=0
-} 
--- src/app/models/Recomendacion.ts
+++
@@ -1,9 +0,0 @@
-import { Meta } from './Meta';
-export class Recomendacion{
-    idRecomendacion:number=0
-    meta:Meta=new Meta()
-    descripcion:string=""
-    categoria:string=""
-    fechapublicacion:Date=new Date()
-    fuente:string=""
-}   
